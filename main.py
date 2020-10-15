b67f1f286cc4afb20b2315989e51e56e99639972
Michael Sanford <michaelsanford@users.noreply.github.com>
2020-10-15 11:40:24 -0400

    Update code to reflect normalized schema, add H plots.

--- main.py
+++ main.py
@@ -5,27 +5,25 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 
 QC = "https://kustom.radio-canada.ca/coronavirus/canada_quebec"
+MONTEREGIE = "https://kustom.radio-canada.ca/coronavirus/canada_quebec_monteregie"
 
-last_days = 300
-nth_label = 5
+last_days = 42
+nth_label = 1
 
 with urllib.request.urlopen(QC) as response:
     data = json.load(response)[0]['History']
     df = pd.json_normalize(data)
 
-    # hospital = int(json.load(response)[0]['Hospitalizations'])
-    # icu = int(json.load(response)[0]['IntensiveCare'])
-
     # Cast types
     df['Date'] = pd.to_datetime(df['Date'])
-    numeric_cols = ['Confirmed', 'Deaths', 'Recovered']
+    numeric_cols = ['C', 'D', 'H', 'I', 'R', 'T']
     df[numeric_cols] = df[numeric_cols].apply(pd.to_numeric, errors='coerce')
 
     # MA calculations make more sense with "today" at the end
     df = df[::-1]
     df.set_index('Date')
 
-    df['Active'] = df.Confirmed - df.Recovered - df.Deaths
+    df['Active'] = df.C - df.R - df.D
     df['Change'] = np.round(df['Active'].diff(), decimals=0)
 
     df = df.sort_values(by=['Date'], ignore_index=True)
@@ -39,8 +37,9 @@ with urllib.request.urlopen(QC) as response:
 
     fig = plt.figure(figsize = (18,8))
     # p = plt.plot(df['Active'], label="Active")
-    # plt.plot(df['Recovered'], label="Recovered")
+    # plt.plot(df['R'], label="R")
     plt.plot(df['Change'], label="Change")
+    plt.plot(df['H'], label="Hospitalizations")
     plt.plot(df['MA'], label="7 MA")
     plt.xlabel("Elapsed Days (last %s)" % last_days)
     plt.ylabel("Cases")
